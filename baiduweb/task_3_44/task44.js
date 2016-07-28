b6a7dc4417ceda2b133095284f02f990582e5923
Yuan <yuanmengzhu@internal.baidu.com>
2016-07-28 17:04:37 +0800

    modify

--- baiduweb/task_3_44/task44.js
+++ baiduweb/task_3_44/task44.js
@@ -5,7 +5,7 @@ window.onload = function(){
     	            {"src":'0.jpg'},{"src":'1.jpg'},{"src":'2.jpg'},{"src":'3.jpg'},{"src":'4.jpg'},
     	            {"src":'5.jpg'},{"src":'6.jpg'},{"src":'7.jpg'},{"src":'8.jpg'},{"src":'9.jpg'},
                     {"src":'10.jpg'},{"src":'11.jpg'},{"src":'12.jpg'},{"src":'13.jpg'},{"src":'14.jpg'},
-                    {"src":'15.jpg'},{"src":'16.jpg'},{"src":'17.jpg'},{"src":'18.jpg'},{"src":'19.jpg'},
+                    {"src":'15.jpg'},{"src":'16.jpg'},{"src":'17.jpg'},{"src":'18.jpg'},{"src":'19.jpg'}
     	            ]
     	        }
     window.onscroll= function(){
