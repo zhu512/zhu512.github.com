5ccdbbb429cd110cb58b77bddf69b7d948a5f9d2
Yuan <yuanmengzhu@internal.baidu.com>
2016-07-28 17:02:49 +0800

    modify

--- baiduweb/task_3_45/task45.js
+++ baiduweb/task_3_45/task45.js
@@ -2,33 +2,41 @@ var dataInt = {
     	            "data":[
     	            {"src":'0.jpg'},{"src":'1.jpg'},{"src":'2.jpg'},{"src":'3.jpg'},{"src":'4.jpg'},
     	            {"src":'5.jpg'},{"src":'6.jpg'},{"src":'7.jpg'},{"src":'8.jpg'},{"src":'9.jpg'},
-    	            {"src":'10.jpg'},{"src":'11.jpg'}
+    	            {"src":'10.jpg'},{"src":'11.jpg'},{"src":'12.jpg'},{"src":'13.jpg'},{"src":'14.jpg'},
+    	            {"src":'15.jpg'},{"src":'16.jpg'},{"src":'17.jpg'},{"src":'18.jpg'},{"src":'19.jpg'}
     	            ]
     	        }
 var Gallery = function(container,maxHeight){
-	this.maxHeight = maxHeight || 300;
+	this.maxHeight = maxHeight || 200;
 	this.container = document.getElementById(container);
 	var width = this.container.offsetWidth;
-	this.minRatio = width/this.maxHeight;
-	this.photos = [];
+	this.minRatio = width/this.maxHeight;console.log(this.minRatio);
+	this.pictures = [];
     this.display();
 }
-Gallery.prototype.getRatio =function(data){//这个参数是传递的图片；
+//得到每张图片的宽高比例
+Gallery.prototype.getRatio = function(data){//这个参数是传递的图片；
+
 	var img = new Image();
-    img.src = "../image/"+data.src;
-    var width = img.width,height = img.height;
-    var picRatio = width/height;
-    return picRatio;
+    img.src = "../image/" + data.src;
+    var width = 0, height = 0, picRatio = 0;
+	img.onload = function(){
+    	width = img.width;
+    	height = img.height;
+    	picRatio = width/height;
+    	return picRatio;
+    }
 }
 Gallery.prototype.getRows = function(){
-	var photos = dataInt.data;
+	var pictures = dataInt.data;
 	var relRatio = 0,rowBox= [], pics = [];
-	for(var i=0,len = photos.length;i<len;i++){
-		var pic = photos[i];
-		pics.push(pic); 
+	for(var i=0,len = pictures.length;i<len;i++){
+		var pic = pictures[i];
+		pics.push(pic);
 		var ratio = this.getRatio(pic);
 		relRatio += ratio;
 		if(relRatio > this.minRatio){
+			debugger
 			rowBox.push({
 				relRatio:relRatio,
 				imgs:pics
@@ -36,30 +44,34 @@ Gallery.prototype.getRows = function(){
 			relRatio = 0;
 			pics = [];
 		}
-	} 
+	}
 	if(pics){
 			rowBox.push({
 				relRatio:this.minRatio,
 				imgs:pics
 			})
-		} 
+		}
+		console.log(rowBox);
 	return rowBox;
 }
 Gallery.prototype.display = function(){
 	var container = this.container
-        width = container.offsetWidth;
+        width = container.offsetWidth;console.log(width);
     this.getRows().forEach(function(row){
+
         var frag = document.createDocumentFragment(),
     	    oRow = document.createElement('div'),
     	    oHeight = parseInt(width/row.relRatio)+'px';
     	oRow.className = "rowBox";
     	oRow.style.height = oHeight;
+
         for(var i =0,len = row.imgs.length;i<len;i++){
+
         	var oBox = document.createElement('div'),
         	    oImg = document.createElement('img');
-             
+
         	oBox.className = "box";
-        	oImg.src = "../image/"+row.imgs[i].src;
+        	oImg.src = "../image/" + row.imgs[i].src;
         	oImg.style.cssText = 'width:auto;height:'+oHeight+';';
         	oBox.appendChild(oImg);
         	oRow.appendChild(oBox);
